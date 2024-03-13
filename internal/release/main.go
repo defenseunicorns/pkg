88e85cdd67268388a465fb6e5b84a5ee3b9c84ae
Austin Abro <AustinAbro321@gmail.com>
2024-03-13 21:15:50 +0000

    now getting tags

--- internal/release/main.go
+++ internal/release/main.go
@@ -2,19 +2,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/google/go-github/v52/github"
 	"golang.org/x/mod/modfile"
 )
 
 func cli(module string) error {
+	// Ensure the path is consistent with the go mod
 	cwd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
-	path := filepath.Join( cwd, "..", "..", module, "go.mod")
+	path := filepath.Join(cwd, "..", "..", module, "go.mod")
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		return err
@@ -24,6 +28,31 @@ func cli(module string) error {
 		return err
 	}
 	fmt.Printf("modfile is %v", modFile.Module.Mod.Path)
+
+	// Get the latest version
+	// Configuration
+	owner := "defenseunicorns"
+	repo := "pkg"
+
+	client := github.NewClient(nil)
+
+	// Fetch tags
+	tags, _, err := client.Repositories.ListTags(context.TODO(), owner, repo, &github.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	// Filter tags based on prefix and sort them
+	var filteredTags []string
+	for _, tag := range tags {
+		if strings.HasPrefix(*tag.Name, module) {
+			filteredTags = append(filteredTags, strings.TrimPrefix(*tag.Name, module))
+		}
+	}
+	fmt.Printf("these are the tags %v", filteredTags)
+
+	// Increment the version based on conventional commits
+
 	return nil
 }
 
