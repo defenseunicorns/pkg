c310b8527994f94ecb1d518eb1c4e6b6f7c38090
Austin Abro <AustinAbro321@gmail.com>
2024-03-14 20:16:08 +0000

    kinda working more

--- internal/release/main.go
+++ internal/release/main.go
@@ -4,13 +4,36 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
+	"github.com/Masterminds/semver"
+	"github.com/go-git/go-git/plumbing/storer"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/google/go-github/v52/github"
 	"golang.org/x/mod/modfile"
 )
+
+var commitTypes = []string{
+	"build",
+	"chore",
+	"ci",
+	"docs",
+	"feat",
+	"fix",
+	"perf",
+	"refactor",
+	"revert",
+	"style",
+	"test",
+}
+
+const repoPath = "/home/austin/code/austin-pkg"
 
 func cli(module string) error {
 	// Ensure the path is consistent with the go mod
@@ -27,33 +50,145 @@ func cli(module string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("modfile is %v", modFile.Module.Mod.Path)
+	fmt.Printf("modfile is %v \n", modFile.Module.Mod.Path)
 
 	// Get the latest version
 	// Configuration
-	owner := "defenseunicorns"
-	repo := "pkg"
-
-	client := github.NewClient(nil)
-
 	// Fetch tags
-	tags, _, err := client.Repositories.ListTags(context.TODO(), owner, repo, &github.ListOptions{})
+	// Filter tags based on prefix and sort them
+	filteredTags, err := getTagsGit(module)
 	if err != nil {
 		return err
 	}
 
-	// Filter tags based on prefix and sort them
+	// Increment the version based on conventional commits
+	vs := make([]*semver.Version, len(filteredTags))
+	for i, r := range filteredTags {
+		v, err := semver.NewVersion(r)
+		if err != nil {
+			return err
+		}
+		vs[i] = v
+	}
+
+	sort.Sort(semver.Collection(vs))
+
+	fmt.Printf("this is the semver collection %v\n", vs)
+	latest := vs[len(vs)-1]
+
+	getCommitMessagesFromLastTag(latest, module)
+
+	// Discover if the latest change is a major, minor, or patch
+	// I will want the latest tag that matters, then I will want all the commits
+	// between that tag and the current tag that used the include
+
+	return nil
+}
+
+func getCommitMessagesFromLastTag(version *semver.Version, module string) ([]string, error) {
+	r, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get repo: %w", err)
+	}
+
+	compareTag := fmt.Sprintf("%s/%s", module, version.Original())
+	fmt.Printf("this is the compare tag %s\n", compareTag)
+	latestTagRef, err := r.Tag(compareTag)
+	if err != nil {
+		log.Fatalf("Failed to get tag: %s", err)
+	}
+
+	// Get the commit object for this tag
+	tagCommit, err := r.CommitObject(latestTagRef.Hash())
+	if err != nil {
+		log.Fatalf("Failed to get commit from tag: %s", err)
+	}
+
+	headRef, err := r.Head()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get reference: %w", err)
+	}
+
+	// Get the commit object for HEAD
+	headCommit, err := r.CommitObject(headRef.Hash())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get commit: %w", err)
+	}
+
+	// Use Log to get all commits from HEAD to the tag's commit
+	pathPrefix := fmt.Sprintf("%s/", module)
+	commits, err := r.Log(&git.LogOptions{
+		From: headCommit.Hash,
+		PathFilter: func(path string) bool {
+			return strings.HasPrefix(path, pathPrefix)
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get commits: %w", err)
+	}
+	var commitMessages []string
+	commits.ForEach(func(c *object.Commit) error {
+		if c.Hash == tagCommit.Hash {
+			// Once we reach the tag's commit, stop iterating
+			return storer.ErrStop
+		}
+		commitMessages = append(commitMessages, c.Message)
+		return nil
+	})
+
+	for _, v := range commitMessages {
+		fmt.Printf("new message %s\n",v)
+	}
+	return commitMessages, nil
+}
+
+func getTagsGit(module string) ([]string, error) {
+
+	// TODO change this
+	r, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open repository: %w", err)
+	}
+
+	// List all tag references
+	tagRefs, err := r.Tags()
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch tags: %w", err)
+	}
+
 	var filteredTags []string
+	tagPrefix := fmt.Sprintf("refs/tags/%s/", module)
+	err = tagRefs.ForEach(func(ref *plumbing.Reference) error {
+		if strings.HasPrefix(string(ref.Name()), tagPrefix) {
+			filteredTags = append(filteredTags, strings.TrimPrefix(string(ref.Name()), tagPrefix))
+		}
+		return nil
+	})
+	fmt.Printf("these are the filtered tag %v\n", filteredTags)
+
+	return filteredTags, err
+}
+
+func getTagsGithub(module string) ([]string, error) {
+	owner := "austinabro321"
+	repo := "pkg"
+
+	client := github.NewClient(nil)
+
+	tags, _, err := client.Repositories.ListTags(context.TODO(), owner, repo, &github.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	var filteredTags []string
+	tagPrefix := fmt.Sprintf("%s/", module)
 	for _, tag := range tags {
-		if strings.HasPrefix(*tag.Name, module) {
-			filteredTags = append(filteredTags, strings.TrimPrefix(*tag.Name, module))
+		if strings.HasPrefix(*tag.Name, tagPrefix) {
+			filteredTags = append(filteredTags, strings.TrimPrefix(*tag.Name, tagPrefix))
 		}
 	}
 	fmt.Printf("these are the tags %v", filteredTags)
-
-	// Increment the version based on conventional commits
-
-	return nil
+	return filteredTags, nil
 }
 
 func main() {
