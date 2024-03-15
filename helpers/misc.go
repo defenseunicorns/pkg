62da8f233179e3515e76930c2fc51222d77dba43
Austin Abro <AustinAbro321@gmail.com>
2024-03-15 17:28:27 +0000

    another function to test releases

--- helpers/misc.go
+++ helpers/misc.go
@@ -29,6 +29,8 @@ func Retry(fn func() error, retries int, delay time.Duration, logger func(format
 		time.Sleep(backoff)
 	}
 
+	
+
 	return err
 }
 
@@ -36,7 +38,7 @@ func Retry(fn func() error, retries int, delay time.Duration, logger func(format
 func MergeMap[T any](m1, m2 map[string]T) (r map[string]T) {
 	r = map[string]T{}
 
-	
+
 
 	for key, value := range m1 {
 		r[key] = value
