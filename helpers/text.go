c4961add08b86d101a9dc84f677dced659fb350d
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2024-03-22 16:43:47 -0400

    refactor: move string.go into text.go (#33)

--- helpers/text.go
+++ helpers/text.go
@@ -73,3 +73,12 @@ func IsTextFile(path string) (bool, error) {
 	// Returns true if both front and back show they are text
 	return isTextCheck[0] && isTextCheck[1], nil
 }
+
+// First30last30 returns the source string that has been trimmed to 30 characters at the beginning and end.
+func First30last30(s string) string {
+	if len(s) > 60 {
+		return s[0:27] + "..." + s[len(s)-26:]
+	}
+
+	return s
+}
