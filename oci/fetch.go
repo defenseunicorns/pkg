d1ef56f08b7916caa7dccfbdb1b0a603411dd2f4
Austin Abro <AustinAbro321@gmail.com>
2024-03-25 21:05:21 +0000

    only put one package comment

--- oci/fetch.go
+++ oci/fetch.go
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024-Present Defense Unicorns
 
-// Package oci contains functions for interacting with artifacts stored in OCI registries.
 package oci
 
 import (
