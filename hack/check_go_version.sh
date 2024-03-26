ce5289067d792fe00f18a62c32521d3b36064b6a
Austin Abro <AustinAbro321@gmail.com>
2024-03-26 14:11:21 +0000

    add script

---
+++ hack/check_go_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+first_version=""
+# Find and iterate over all go.mod files, excluding the vendor directory
+find . -name go.mod | while read -r mod; do
+    current_version=$(grep 'go 1.' "$mod" | cut -d ' ' -f 2)
+
+    if [[ -z "$first_version" ]]; then
+      first_version=$current_version
+    elif [[ "$current_version" != "$first_version" ]]; then
+      echo "Inconsistency found: $mod uses Go version $current_version, differs another found version $first_version."
+      exit 1
+    fi
+done
+
+echo "All modules use the same Go version: $first_version."
