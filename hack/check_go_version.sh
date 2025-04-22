69bb3fe0c64a64009c7289ecc2e04649c4fe1806
Harry Randazzo <harry@razzle.cloud>
2025-04-22 12:17:39 -0500

    use subshell
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- hack/check_go_version.sh
+++ hack/check_go_version.sh
@@ -3,16 +3,15 @@
 set -euo pipefail
 
 first_version=""
-# Find and iterate over all go.mod files, excluding the vendor directory
-find . -name go.mod | while read -r mod; do
+while read -r mod; do
     current_version=$(grep '^go 1\.' "$mod" | cut -d ' ' -f 2)
 
     if [[ -z "$first_version" ]]; then
-      first_version=$current_version
+        first_version=$current_version
     elif [[ "$current_version" != "$first_version" ]]; then
-      echo "Inconsistency found: $mod uses Go version $current_version, this differs from another found version $first_version."
-      exit 1
+        echo "Inconsistency found: $mod uses Go version $current_version, this differs from another found version $first_version."
+        exit 1
     fi
-done
+done < <(find . -name go.mod -not -path '*/vendor/*')
 
 echo "All modules use the same Go version: $first_version."
